8374554f02666268e312697ca35290fec7af9ad7
Dani <s.leslie94@gmail.com>
2023-03-20 14:24:59 -0400

    add tv data, show on DOM

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+
+document.querySelector('#getShow').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
 
-  fetch(url)
+  const choice = document.querySelector('#showName').value.toLowerCase();
+  console.log(choice);
+  const image = document.querySelector('#poster');
+  const title = document.querySelector('#title');
+  const showStart = document.querySelector('#showStart');
+  const showEnd = document.querySelector('#showEnd');
+  const avgRuntime = document.querySelector('#avgRuntime');
+  const description = document.querySelector('#description');
+  const seasonCount = document.querySelector('#seasonCount');
+  
+
+
+  let url = `https://api.tvmaze.com/search/shows?q=${choice}`;
+
+    fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        }
-       
-        document.querySelector('h3').innerText = data.explanation
+        console.log(data);
+
+        let seasonID = `https://api.tvmaze.com/shows/${data[0].show.id}/seasons`;
+
+        title.textContent = data[0].show.name;
+        image.src = data[0].show.image.original;
+        let startDate = new Date(data[0].show.premiered).getFullYear();
+        showStart.textContent = startDate;
+        avgRuntime.textContent = data[0].show.averageRuntime;
+        description.innerHTML = data[0].show.summary;
+
+
+if(data[0].show.ended){
+    showEnd.textContent = data[0].show.ended;
+} else {
+   showEnd.textContent = 'Now'; 
+}
+    fetch(seasonID)
+    .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data);
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-}
+})}
+
+
 
